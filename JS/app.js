1efdd5eb40640ea4626c016da82bb04ae3e050e4
Ryan Andrade <andmryan@gmail.com>
2025-02-25 20:52:22 -0500

    Pseudocode + Initial Planning.

---
+++ JS/app.js
@@ -0,0 +1,32 @@
+// Things I will be using to make the game:
+//   1. Arrays
+//   2. Queryselectors
+//   3. Event Handlers
+//   4. Math.random()
+//   6. CSS/Grid and/or Flexbox
+
+
+// What I want the game to do:
+//  1. I want the game to exist in a grid of at least 5x5, but at most 7x7, where the different blocks have different arrays and 'difficulty' starting with the easiest being the top left and the hardest being the bottom right.
+//  2. I would like the game to recognize which block the player clicks in and to recognize which array corresponds to each box.
+//  3. I would also like it to keep track of caught fish, amount of points, and casts remaining. It would be nice to be able to display the fish caught, but only the points accumulated and casts remaining are necessary. 
+    // 3b. I would also like to have a high score table so you can play against yourself (or another player) on the same screen.
+//  4. I need a reset button.
+//  5. If I have a display of the fish caught, I need to decide if it's going to be always on or only after you get to the results page.
+
+// Things I need to do:
+// 1. Initialize the game
+//  Empty/clear the caught arrays
+    //Empty/clear the score totals
+    //Refill the remaining casts
+    //[maybe] Print the scores to the high score list
+// 2. Start at Player one
+//  fish in the appropriate array
+    // subtract from the remaining casts
+    // display fish caught + its point value
+    // add new points to total points
+    // [maybe] display fish caught in list
+// 3. Switch to player two after player two makes a move.
+    // Repeat steps from player two but for player two.
+// 5. Switch to player one after player two makes a move.
+// 6. End the game when both players are out of casts.
